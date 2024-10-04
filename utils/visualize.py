20f67da55b7748a87cbdb7eb2e26483e0f0ad62b
Erbil Can Artun <49557189+erbilcanartun@users.noreply.github.com>
2024-10-04 17:44:30 +0300

    Add indicator_plot method to draw static and interactive charts

--- utils/visualize.py
+++ utils/visualize.py
@@ -1,10 +1,126 @@
-from bokeh.layouts import column
 from bokeh.models import LinearAxis, Range1d
 from bokeh.plotting import figure, show, output_file
+from bokeh.layouts import column
 from bokeh.io import output_notebook, reset_output
+
+import matplotlib.pyplot as plt
 
 import numpy
 import talib
+
+from scipy.spatial.distance import pdist
+
+
+def indicator_plot(ticker, price_data_series, price_data_frame, indicator_name, indicator_data, interactive=True, inline=True, fig_width = 1200, fig_height = 500):
+
+    if interactive:
+        interactive_plot(ticker, price_data_frame, indicator_name, indicator_data, inline = inline, fig_width = fig_width, fig_height = fig_height)
+    else:
+        static_plot(ticker, price_data_series, indicator_name, indicator_data)
+
+    return None
+
+def static_plot(ticker, price_data, indicator_name, indicator_data):
+
+    fig, ax = plt.subplots(figsize=(13, 6))
+    plt.style.use('classic')
+    fig.set_facecolor('white')
+    lw, fs = 1, 14
+    plt.rc('lines', linewidth=lw)
+    plt.rc('axes', linewidth=lw)
+    plt.rcParams['font.family'] = 'Arial'
+    plt.rcParams.update({'mathtext.default':'regular'})
+
+    ax.plot(price_data, ls='-',color='blue', lw=.7)
+    ax.tick_params(axis="x", direction="in", labelcolor='black', width=lw, length=4)
+    ax.tick_params(axis="y", direction="in", labelcolor='blue', width=lw, length=4)
+    ax.set_xlabel("Date", fontsize=fs, )
+    ax.set_ylabel(ticker, fontsize=fs)
+
+    ax_indicator = ax.twinx()
+    ax_indicator.plot(indicator_data.indicator, color='green', lw=.8)
+    ax_indicator.tick_params(axis="both", direction="in", left=True, labelcolor='green', width=lw, length=4)
+    ax_indicator.set_ylabel(indicator_name, fontsize=fs)
+    plt.show()
+    return None
+
+def interactive_plot(ticker, price_data, indicator_name, indicator_data, inline = True, fig_width = 1200, fig_height = 500):
+
+    fs = '12pt'
+
+    if inline:
+        reset_output()
+        output_notebook(notebook_type='jupyter', hide_banner=True)
+    else:
+        reset_output()
+        output_file("candle_chart.html")
+
+    # Increasing and decreasing candles
+    inc = price_data.Close > price_data.Open
+    dec = price_data.Open > price_data.Close
+
+    # Candlestick width
+    # candle_body_width = Half of the trading time in miliseconds
+    candle_body_width = (price_data.index[1] - price_data.index[0]).total_seconds() * 1000 / 2
+
+    # Prepare the candlestick chart
+    tools = "pan, wheel_zoom, reset"
+    candChart = figure(title="Price Chart", x_axis_type="datetime", tools=tools,
+                       toolbar_location="right", toolbar_sticky=False,
+                       width=fig_width, height=fig_height)
+    # Green bars
+    candChart.segment(x0=price_data.index[inc], y0=price_data.High[inc],
+                      x1=price_data.index[inc], y1=price_data.Low[inc], color="green")
+    candChart.vbar(x=price_data.index[inc], bottom=price_data.Open[inc],
+                   top=price_data.Close[inc], width=candle_body_width,
+                   fill_color="green", line_color="green")
+    # Red bars
+    candChart.segment(x0=price_data.index[dec], y0=price_data.High[dec],
+                      x1=price_data.index[dec], y1=price_data.Low[dec], color="red")
+    candChart.vbar(x=price_data.index[dec], bottom=price_data.Close[dec],
+                   top=price_data.Open[dec], width=candle_body_width,
+                   fill_color="red", line_color="red")
+
+    # Set the axis labels
+    #candChart.xaxis.axis_label = "Date"
+    candChart.xaxis.axis_label_text_font_size = fs
+    candChart.xaxis.axis_label_text_font_style = 'normal'
+    #candChart.xaxis.axis_label_text_font = "times"
+    #candChart.xaxis.axis_label_text_color = "black"
+
+    candChart.yaxis.axis_label = ticker
+    candChart.yaxis.axis_label_text_font_size = fs
+    candChart.yaxis.axis_label_text_font_style = 'normal'
+    #candChart.yaxis.axis_label_text_font = "times"
+    #candChart.yaxis.axis_label_text_color = "black"
+
+    #=============================== VOLUME CHART ================================#
+    #volChart = figure(x_axis_type="datetime", width=fig_width, height=200)
+    #volChart.vbar(dataframe.index[inc], width=width, top=dataframe.Volume[inc], fill_color="green", line_color="green", alpha=0.8)
+    #volChart.vbar(dataframe.index[dec], width=width, top=dataframe.Volume[dec], fill_color="red", line_color="red", alpha=0.8)
+    #volChart.xaxis.axis_label="Date"
+    #volChart.yaxis.axis_label="Volume"
+
+    #============================== INDICATOR CHART ==============================#
+    indicatorChart = figure(title="Indicator Chart", x_axis_type="datetime", x_range=candChart.x_range,
+                            tools=tools, toolbar_location="right", toolbar_sticky=False,
+                            width=fig_width, height=300)
+    indicatorChart.line(x=indicator_data.index, y=indicator_data.indicator, line_color="purple",
+                        line_width=1.5, name=indicator_name, legend_label=indicator_name)
+
+    indicatorChart.xaxis.axis_label="Date"
+    indicatorChart.xaxis.axis_label_text_font_size = fs
+    indicatorChart.xaxis.axis_label_text_font_style = 'normal'
+    indicatorChart.yaxis.axis_label = indicator_name
+    indicatorChart.yaxis.axis_label_text_font_size = fs
+    indicatorChart.yaxis.axis_label_text_font_style = 'normal'
+
+    indicatorChart.legend.location = "top_left"
+    indicatorChart.legend.click_policy = "hide"
+
+    layout = column(candChart, indicatorChart)
+    show(layout)
+    return None
 
 def which_indicator(indicator_name:str):
 
@@ -18,7 +134,7 @@ def which_indicator(indicator_name:str):
         return talib.STOCHRSI
 
 
-def candle_chart(ticker, start_date, end_date, data,
+def candle_chart(ticker, data,
                  indicators_on: dict = {'SMA':None, 'EMA':None},
                  indicators_off: dict = {'RSI':None, 'STOCHRSI':None},
                  inline=True, fig_width = 1200, fig_height = 500):
